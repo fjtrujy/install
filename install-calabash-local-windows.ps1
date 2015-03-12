77ce267fdb36a79a2bed5f92373580dae0b2cc62
Tom Opgenorth <tom@opgenorth.net>
2015-03-12 09:45:12 -0600

    Enhancements to get xamarin-test-cloud gem installed.
    See https://github.com/xamarin/test-cloud-frontend/issues/1165.
    * Need to update the system RubyGems
    * Need to install ffi seperately.

--- install-calabash-local-windows.ps1
+++ install-calabash-local-windows.ps1
@@ -1,40 +1,54 @@
+Write-Host ' '
 Write-Host 'Installing Calabash on Windows:'
 
 # STEP 1: Check the version of Ruby installed.
 $result = & "ruby" "-v"
 Write-Host "  Ruby version installed: " $result
 
-# STEP 2: Create the Calabash directory if necessary.
 $calabashDir = "$env:USERPROFILE\calabash\"
 if (Test-Path $calabashDir)
 {
-	Write-Host "  The directory $calabashDir already exists - aborting install."
+	Write-Host " "
+	Write-Host "The directory $calabashDir already exists - aborting install."
+	Write-Host "If you want to run this script again, please delete that directory."
+	Write-Host " "
 }
 else 
 {
+	# STEP 2: Create the Calabash directory.
+	Write-Host "  Creating the Calabash directory at $calabashDir."	
 	New-Item $calabashDir -ItemType directory | Out-Null
-	Write-Host "  Creating the Calabash directory at $calabashDir"	
-
-# Not sure we need/should do this on Windows.	
-#	[Environment]::SetEnvironmentVariable("GEM_HOME", $calabashDir, "user")
-#	[Environment]::SetEnvironmentVariable("GEM_PATH", $calabashDir, "user")
-#	Write-Host "  Setting GEM_HOME and GEM_PATH to $calabashDir."
-
+	Write-Host " "
 
 	# STEP 3: Download the SSL Certificate and set the SSL_CERT_FILE 
 	# environment variable for the process.
 	$cacertFile = "$calabashDir\cacert.pem"
+	Write-Host "  Downloading SSL certificate to $cacertFile."
 	Invoke-WebRequest 'http://curl.haxx.se/ca/cacert.pem' -OutFile $cacertFile
-	[Environment]::SetEnvironmentVariable("SSL_CERT_FILE", $cacertFile, "user")
+	Write-Host " "
 
-	# STEP 4: Install the gem
-	Write-Host "  Installing xamarin-test-cloud..."
+	Write-Host "  Set the environment variable SSL_CERT_FILE=$cacertFile."
+	[Environment]::SetEnvironmentVariable("SSL_CERT_FILE", $cacertFile, "user")
+	[Environment]::SetEnvironmentVariable("SSL_CERT_FILE", $cacertFile, "process")
+	Write-Host " "
+
+	# STEP 4: Update the version of RubyGems 
+	Write-Host "  Performing a system update of RubyGems:"
+	& "gem" "update" "--system"
+	Write-Host " "
+
+	# STEP 5: Install the gems
+	Write-Host "    Installing ffi..."
+	& "gem" "install" "ffi" "--platform=ruby" "--no-ri" "--no-rdoc"
+	Write-Host " "
+	
+	Write-Host "    Installing xamarin-test-cloud..."
 	& "gem" "install" "xamarin-test-cloud" "--platform=ruby"  "--no-ri" "--no-rdoc"
 	Write-Host " "
 
-	Write-Host "  Installing calabash-android..."
+	Write-Host "    Installing calabash-android..."
 	& "gem" "install" "calabash-android" "--platform=ruby"  "--no-ri" "--no-rdoc"
 	Write-Host " "
+
+	Write-Host "Finished installing Calabash on Windows."
 }
-
-
