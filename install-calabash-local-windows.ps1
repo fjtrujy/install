e4420bcb51c19a19981dcd97e7b8ea0865d67b94
Jonathan Stoneman <jon@stoneman.me.uk>
2015-11-18 11:33:09 +0000

    Windows sanity in place of *nix parity
    - We don't need a $HOME\calabash\gems folder on Windows, gems can be installed into the default location
    - Also removed the cacert folder.  Recent versions of ruby contain the cert and we can place it in the default ssl certs folder in older versions
    - $HOME\.calabash is created for use by calabash
    - Console output updated to better match *nix script

--- install-calabash-local-windows.ps1
+++ install-calabash-local-windows.ps1
@@ -1,28 +1,5 @@
 Param([switch]$ReplaceCalabashUserFiles)
 
-Function Remove-Directory([string]$DirectoryToRemove)
-{
-    if (Test-Path $DirectoryToRemove)
-    {
-        if (!($ReplaceCalabashUserFiles))
-        {
-            do {
-                $deleteDir = Read-Host "The directory $DirectoryToRemove already exists.  Do you want to delete it? (Y | N)"
-                if ($deleteDir -eq "N")
-                {
-                    Write-Host "  Unable to continue, exiting."
-                    Return $False
-                }
-            
-            } while ($deleteDir -notmatch "[Y]")
-        }
-
-        Remove-Item -Recurse -Force $DirectoryToRemove
-    }
-
-    Return $True
-}
-
 Write-Host ' '
 Write-Host 'Installing Calabash on Windows:'
 Write-Host " "
@@ -46,7 +23,7 @@ else
 Write-Host "  "
 
 # STEP 3: Create the Calabash directory if required.
-$calabashDir = "$env:USERPROFILE\calabash"
+$calabashDir = "$env:USERPROFILE\.calabash"
 if (!(Test-Path $calabashDir)) 
 {
 	Write-Host "  Creating the Calabash directory at $calabashDir."	
@@ -54,52 +31,36 @@ if (!(Test-Path $calabashDir))
 	Write-Host " "
 }
 
-# STEP 4: Removing any existing Certificates directory.
-$cacertDir = "$calabashDir\certs"
-if (!(Remove-Directory -DirectoryToRemove $cacertDir))
+# STEP 4: Get the SSL certificates directory
+$rubygems = iex "gem which rubygems"
+if (!($rubygems.EndsWith("rubygems.rb")))
 {
+    Write-Host " "
+    Write-Host "  Unable to locate rubygems install directory!  Exiting..."
+    Write-Host " "
     Exit
 }
+$rubygemsDir = $rubygems.Substring(0, $rubygems.Length - 3)
+$sslCertsDir = "$rubygemsDir\ssl_certs"
 
-# STEP 5: Create a new Certificates directory.
-Write-Host "  Creating the Calabash cert directory at $cacertDir."	
-New-Item $cacertDir -ItemType directory | Out-Null
-Write-Host " "
-
-# STEP 6: Download the SSL Certificate and set the SSL_CERT_FILE 
-# environment variable for the process.
-$cacertFile = "$cacertDir\cacert.pem"
-Write-Host "  Downloading SSL certificate to $cacertFile."
-Invoke-WebRequest 'http://curl.haxx.se/ca/cacert.pem' -OutFile $cacertFile
-Write-Host " "
-
-Write-Host "  Set the environment variable SSL_CERT_FILE=$cacertFile."
-[Environment]::SetEnvironmentVariable("SSL_CERT_FILE", $cacertFile, "user")
-[Environment]::SetEnvironmentVariable("SSL_CERT_FILE", $cacertFile, "process")
-Write-Host " "
-
-# STEP 7: Remove any existing Gems directory.
-$gemsDir = "$calabashDir\gems"
-if (!(Remove-Directory -DirectoryToRemove $gemsDir))
+# STEP 5: Download the SSL Certificate if required.
+$cacertFile = "$sslCertsDir\AddTrustExternalCARoot.pem"
+if (!(Test-Path $cacertFile))
 {
-    Exit
+    Write-Host "  Downloading SSL certificate to $cacertFile."
+    Invoke-WebRequest 'https://github.com/rubygems/rubygems/raw/master/lib/rubygems/ssl_certs/AddTrustExternalCARoot.pem' -OutFile $cacertFile
+    Write-Host " "
 }
 
-# STEP 8: Create the Gems directory.
-Write-Host "  Creating the Calabash gems directory at $gemsDir."
-New-Item $gemsDir -ItemType directory | Out-Null
-Write-Host " "
-
-# STEP 9: Update the version of RubyGems 
+# STEP 6: Update the version of RubyGems 
 Write-Host "  Performing a system update of RubyGems:"
 & "gem" "update" "--system"
 Write-Host " "
 
-# STEP 10: set the GEN_HOME and GEN_PATH environment variables.
-$Env:GEM_HOME = $gemsDir;
-$Env:GEM_PATH = $gemsDir;
+# STEP 7: Install the gems
+Write-Host "  Installing Calabash... This could take a few minutes"
+Write-Host " "
 
-# STEP 11: Install the gems
 Write-Host "    Installing xamarin-test-cloud..."
 & "gem" "install" "xamarin-test-cloud" "--platform=ruby"  "--no-document"
 Write-Host " "
@@ -108,4 +69,10 @@ Write-Host "    Installing calabash-android..."
 & "gem" "install" "calabash-android" "--platform=ruby"  "--no-document"
 Write-Host " "
 
-Write-Host "Finished installing Calabash on Windows."
+Write-Host "Done Installing!"
+Write-Host " "
+Write-Host "You can always uninstall using:"
+Write-Host " "
+Write-Host "  gem uninstall xamarin-test-cloud"
+Write-Host "  gem uninstall calabash-android"
+
