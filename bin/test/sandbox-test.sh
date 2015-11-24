b30125252449708ae11da0912acc24d514c48580
Christopher Fuentes <chris.fuentes@xamarin.com>
2015-11-24 15:09:32 -0800

    RM other system check, some logs for test

--- bin/test/sandbox-test.sh
+++ bin/test/sandbox-test.sh
@@ -7,16 +7,20 @@ DROID=$( { echo "calabash-android version >&2" |  calabash-sandbox 1>/dev/null; 
 IOS=$( { echo "calabash-ios version >&2" | calabash-sandbox 1>/dev/null; } 2>&1)
 gem_home=$( { echo "echo \$GEM_HOME >&2" | calabash-sandbox 1>/dev/null; } 2>&1)
 
+echo "Testing calabash-android version"
 if [ "${DROID}" != "0.5.15" ]; then
   echo "calabash-android version ($DROID) should be 0.15.5"
   exit 1
-elif [ "${IOS}" != "0.16.4" ]; then
+fi
+
+echo "Testing calabash-ios version"
+if [ "${IOS}" != "0.16.4" ]; then
   echo "calabash-ios version ($IOS) should be 0.16.4"
   exit 2
-elif [ "${gem_home}" != "${HOME}/.calabash/sandbox/Gems" ]; then
+fi
+
+echo "Ensuring sandbox GEM_HOME properly set"
+if [ "${gem_home}" != "${HOME}/.calabash/sandbox/Gems" ]; then
   echo "Gem Home should be ${HOME}/.calabash/sandbox/Gems; Got $gem_home"
   exit 3
-elif [ "${gem_home}" == "${GEM_HOME}" ]; then
-  echo "Sandbox gem home ($gem_home) should be different than system gem home ($GEM_HOME)"
-  exit 4
 fi
