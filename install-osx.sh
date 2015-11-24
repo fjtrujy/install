faa16637ed926f1609ca75a37d799567ef852f22
Christopher Fuentes <chris.fuentes@xamarin.com>
2015-11-24 15:02:21 -0800

    Use local bundler

--- install-osx.sh
+++ install-osx.sh
@@ -33,7 +33,7 @@ echo "Preparing sandbox..."
 
 #Download the gems and their dependencies
 echo "Installing gems, this may take a little while..."
-$GEM install bundler --no-rdoc --no-ri
+$GEM install bundler --no-rdoc --no-ri --bindir ${GEM_HOME}/bin
 
 #ad hoc Gemfile
 echo "source 'https://rubygems.org'" > "${SANDBOX}/Gemfile"
@@ -42,7 +42,7 @@ echo "gem 'calabash-android', '>= 0.5.15', '< 1.0'" >> "${SANDBOX}/Gemfile"
 echo "gem 'xamarin-test-cloud', '~> 1.0'" >> "${SANDBOX}/Gemfile"
 
 #TODO Do we need to clear defaults for bundler...?
-cd "${SANDBOX}" && bundle install --path=${GEM_HOME} --binstubs=${GEM_HOME}/bin
+cd "${SANDBOX}" && ${GEM_HOME}/bin/bundle install --path=${GEM_HOME} --binstubs=${GEM_HOME}/bin
 
 echo "Done!"
 echo -e "Execute '\033[0;32m calabash-sandbox \033[00m' to get started! "
