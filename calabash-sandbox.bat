5711c5fbe3ed73163d8df3002ecbd54201dd655e
Jonathan Stoneman <jon@stoneman.me.uk>
2015-12-18 22:35:42 +0000

    Updating files referenced by download scripts straight to master
    These files have not be officially made public so am updating them in advance of https://github.com/calabash/install/pull/36 being merged as they are referenced by other scripts via their master URLs.

--- calabash-sandbox.bat
+++ calabash-sandbox.bat
@@ -1,20 +1,21 @@
 @echo off
 
-set SCRIPT_VERSION=0.1.1
-
 setlocal EnableDelayedExpansion
 
-title Calabash Sandbox
-color 0a
-cls
+set SCRIPT_VERSION=0.1.2
 
-echo.
-echo Calabash sandbox version: %SCRIPT_VERSION%
-echo.
+if "%1" == "version" (
+  echo.
+  echo Calabash sandbox version: %SCRIPT_VERSION%
+  GOTO:EOF
+)
 
+for /f "delims=" %%i in ('echo %CMDCMDLINE% ^| findstr "calabash-sandbox.bat"') do set powershellCommand=%%i
+
+set CALABASH_SANDBOX=%USERPROFILE%\.calabash\sandbox
 set CALABASH_RUBY_VERSION=ruby-2.1.5-p273
-set CALABASH_RUBY_PATH=%USERPROFILE%\.calabash\sandbox\Rubies\%CALABASH_RUBY_VERSION%\bin
-set CALABASH_GEM_HOME=%USERPROFILE%\.calabash\sandbox\Gems
+set CALABASH_RUBY_PATH=%CALABASH_SANDBOX%\Rubies\%CALABASH_RUBY_VERSION%\bin
+set CALABASH_GEM_HOME=%CALABASH_SANDBOX%\Gems
 
 set GEM_HOME=%CALABASH_GEM_HOME%
 set GEM_PATH=%CALABASH_GEM_HOME%
@@ -31,10 +32,71 @@ for %%a in ("%PATH:;=" "%") do (
   )
 )
 
+if "%1" == "update" (
+  echo.
+  echo Updating gems...
+  
+  set CurrentDirectory=%cd%
+  cd %CALABASH_SANDBOX% > nul
+  
+  set GemfileExists=0
+  if exist Gemfile (
+    set GemfileExists=1
+    for /f "delims=" %%i in ('"forfiles /m Gemfile /c "cmd /c echo @fdate @ftime" "') do set OriginalGemfileDate=%%i	
+  )
+  
+  bitsadmin.exe /transfer DownloadingGemfile https://raw.githubusercontent.com/calabash/install/master/Gemfile.Windows !CALABASH_SANDBOX!\Gemfile > nul
+    
+  if !GemfileExists!==0 (
+    if not exist Gemfile (
+      echo.
+      echo Unable to download newest Gemfile, please try again.
+      cd %CurrentDirectory% > nul
+      GOTO:EOF
+    )
+  ) else (
+    for /f "delims=" %%i in ('"forfiles /m Gemfile /c "cmd /c echo @fdate @ftime" "') do set CurrentGemfileDate=%%i	
+    if "!OriginalGemfileDate!" == "!CurrentGemfileDate!" (
+      echo Unable to download newest Gemfile, please try again.
+	  cd %CurrentDirectory% > nul
+      GOTO:EOF
+    )
+  )
+  
+  cmd /C "calabash-sandbox > nul && bundle update > nul 2>&1"
+  for /f "delims=" %%i in ('cmd /C "calabash-sandbox > nul && calabash-android version 2>&1" ') do set AndroidVersion=%%i
+  for /f "delims=" %%i in ('cmd /C "calabash-sandbox > nul && test-cloud version 2>&1" ') do set TestCloudVersion=%%i
+  
+  echo.
+  echo Done^^! Now the sandbox contains:
+  
+  echo calabash-android:   !AndroidVersion!
+  echo xamarin-test-cloud: !TestCloudVersion! 
+  
+  cd %CurrentDirectory% > nul
+  GOTO:EOF
+)
+
+title Calabash Sandbox
+if "%powershellCommand%"=="" (
+  color 0b
+)
+cls
+
+echo.
+echo Calabash sandbox version: %SCRIPT_VERSION%
+echo.
+
 endlocal & (
   set "PATH=%CALABASH_RUBY_PATH%;%GEM_HOME%\bin;%CALABASH_SANDBOX_PATH%"
   set GEM_HOME=%GEM_HOME%
   set GEM_PATH=%GEM_PATH%
+  
+  set PROMPT=[calabash] $p$g
+  
+  if not "%powershellCommand%"=="" (
+    cmd /k echo type 'exit' to return to powershell when done
+  )
 )
 
 
