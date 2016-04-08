bccfd1394767a2abdbfd914bb6d26b659f71b67e
Christopher Fuentes <chris.fuentes@xamarin.com>
2016-04-08 09:52:23 -0700

    Updated OS X publish script to use sandbox gem home

--- bin/publish/osx.sh
+++ bin/publish/osx.sh
@@ -23,6 +23,7 @@ function validate_cmd {
 }
 
 banner "Updating Calabash Sandbox Gems"
+info "If you're not a maintainer, don't run this script!"
 
 VERSION_LINE=`cat calabash-sandbox | grep SCRIPT_VERSION=`
 VERSION=`echo $VERSION_LINE | cut -d= -f2 | cut -d\" -f2 | cut -d\" -f1`
@@ -36,29 +37,28 @@ banner "Updating sandbox version to $NEW_VERSION"
 sed -i .bak "s/${VERSION}/${NEW_VERSION}/" calabash-sandbox
 
 info "Retrieving Gemfile"
-cp "./Gemfile.OSX" Gemfile
-GEMS_DIR=.Gems
+SANDBOX="${HOME}/.calabash/sandbox"
+cp "./Gemfile.OSX" "${SANDBOX}/Gemfile"
+
 validate_cmd $? "copying Gemfile.OSX" 10
 
-info "Creating Gems Dir"
-mkdir $GEMS_DIR
-validate_cmd $? "creating Gems Dir $GEMS_DIR" 2
+if [ ! -d "${SANDBOX}" ]; then 
+  error "Sandbox dir does note exist! Make sure you have a sandbox installation first." 11
+fi
+cd "${SANDBOX}"
+GEMS_DIR=Gems
 
 info "Installing Gems"
-GEM_HOME=$GEMS_DIR bundle install
+calabash-sandbox update
 validate_cmd $? "installing gems" 3
 
 info "Zipping Gems"
 zip -qr CalabashGems.zip $GEMS_DIR
 validate_cmd $? "zipping gems" 4
 
-info "Cleaning up old Gems folder" 
-rm -rf $GEMS_DIR
-validate_cmd $? "cleaning up old gems folder" 5
-
 banner "Testing S3 Connection"
 
-info "Verifying Credentials. If you're not a maintainer, don't run this script"
+info "Verifying Credentials"
 aws s3 ls | grep "calabash-files"
 validate_cmd $? "verifying credentials. If you are a maintainer and don't have creds, \
 you should know who to talk to in order to get them." 6
@@ -69,7 +69,6 @@ validate_cmd $? "uploading CalabashGems.zip to S3" 7
 
 info "Cleaning Up"
 rm -rf CalabashGems.zip
-rm Gemfile
 validate_cmd $? "cleaning up" 9
 
 info "All done. Please commit the latest 'calabash-sandbox' file so that users \
